bcb79c3cff3458faa135aa3652923afa00ef02ea
oumayma56 <40735418+oumayma56@users.noreply.github.com>
2018-07-01 13:21:04 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,60 @@
 
 // When size is submitted by the user, call makeGrid()
 
+colorPicker and sizePicker
+const sizePicker = $('#sizePicker');
+const colorPicker = $('#colorPicker');
+
+
+
 function makeGrid() {
 
-// Your code goes here!
+ let tableHeight, tableWidth, grid, table;
+
+ //set the canvas table and get input values
+ table = $('#pixel_canvas');
+ tableHeight = $('#input_height').val();
+ tableWidth = $('#input_width').val();
+
+ //create the canvas table HTML and save it into grid
+ for (let i = 0; i < tableHeight; i++ ){
+   grid += '';
+   for (let j = 0; j < tableWidth; j++) {
+     grid += '';
+   }
+   grid += '';
+ }
+
+ //fill the canvas table with HTML from grid
+ table.html(grid);
 
 }
+
+
+//add a listener for submit button
+sizePicker.submit(function(event){
+ event.preventDefault();
+
+ //create the canvas table
+ makeGrid();
+
+ //add a listener to all  elements in HTML
+ $('td').click(function(event){
+
+   //set the helper clickedCell, get the color from colorPicker and the current color from the clicked  and save it in a variable
+   let clickedCell = event.target;
+   let color = colorPicker.val()
+   let current_color = $(clickedCell).css('background-color');
+
+   //set the background color of the  to color and the border to #dbfffe
+   $(clickedCell).css('background-color', color);
+   $(clickedCell).css('border','#dbfffe solid 1px');
+
+   // check if we're trying to click on the  with the same color that's in it, if we are restore the background color and border color to basic
+   if (current_color === $(clickedCell).css('background-color')){
+     $(clickedCell).css('background-color', '#ffffff');
+     $(clickedCell).css('border','black solid 1px');
+   }
+ });
+});
+
